10f9962355e8d52b6634d40509537af2b6b03986
Orlandd <alvenorlando@gmail.com>
2024-11-12 10:32:27 +0700

    docker

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,8 +1,13 @@
 const tf = require("@tensorflow/tfjs-node");
 
 
+const path = require('path');
+
 async function loadModel() {
-    return tf.loadGraphModel(process.env.MODEL_URL);
+    const modelPath = 'file://' + path.resolve(__dirname, '../../model/model.json');
+    console.log("Loading model from:", modelPath);
+    return tf.loadGraphModel(modelPath);
 }
+
 
 module.exports = loadModel;
--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,10 +1,39 @@
-const { Firestore } = require("@google-cloud/firestore");
+// const { Firestore } = require("@google-cloud/firestore");
+
+// async function storeData(id, data) {
+//   const db = new Firestore();
+
+//   const predictCollection = db.collection("prediction");
+//   return predictCollection.doc(id).set(data);
+// }
+
+const mysql = require('mysql2/promise');
 
 async function storeData(id, data) {
-  const db = new Firestore();
+  // Buat koneksi ke database
+  const connection = await mysql.createConnection({
+    host: 'localhost', // Ganti dengan host database Anda
+    user: 'root',      // Ganti dengan username database Anda
+    password: 'password', // Ganti dengan password database Anda
+    database: 'cancer' // Ganti dengan nama database Anda
+  });
 
-  const predictCollection = db.collection("prediction");
-  return predictCollection.doc(id).set(data);
+  try {
+    // Query untuk menyimpan data
+    const query = `INSERT INTO predictions (id, data) VALUES (?, ?) 
+                   ON DUPLICATE KEY UPDATE data = VALUES(data)`;
+
+    // Eksekusi query dengan nilai id dan data sebagai parameter
+    await connection.execute(query, [id, JSON.stringify(data)]);
+
+    console.log('Data berhasil disimpan');
+  } catch (error) {
+    console.error('Error menyimpan data:', error);
+  } finally {
+    // Tutup koneksi
+    await connection.end();
+  }
 }
+
 
 module.exports = storeData;
